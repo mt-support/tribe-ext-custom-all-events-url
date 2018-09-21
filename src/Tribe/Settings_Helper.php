33e35f48711ec287054aff745db1639a97f7efae
András Guseo <andras@divi-magazine.com>
2018-09-21 22:08:13 +0200

    Fixed textdomain
    Fixed textdomain and updated pot.

--- src/Tribe/Settings_Helper.php
+++ src/Tribe/Settings_Helper.php
@@ -131,7 +131,7 @@ class Tribe__Settings_Helper {
 				$misc_heading = array(
 					'tribeMiscSettings' => array(
 						'type' => 'html',
-						'html' => '<h3>' . esc_html__( 'Miscellaneous Settings', 'tribe-common' ) . '</h3>',
+						'html' => '<h3>' . esc_html__( 'Miscellaneous Settings', 'tribe-ext-custom-all-events-url' ) . '</h3>',
 					),
 				);
 				$fields = Tribe__Main::array_insert_before_key( 'tribe-form-content-end', $fields, $misc_heading );
