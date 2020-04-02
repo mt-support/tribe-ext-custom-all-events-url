392d9d0c655af1e64e1aa31626e5337814b3b190
András Guseo <andras@divi-magazine.com>
2020-04-02 16:58:39 +0200

    Adding the class_exist check
    No other changes, just the tabbing.

--- src/Settings.php
+++ src/Settings.php
@@ -4,132 +4,136 @@ namespace Tribe\Extensions\CustomAllEventsUrl;
 
 use Tribe__Settings_Manager;
 
-/**
- * Do the Settings.
- */
-class Settings {
-
+if ( ! class_exists( Settings::class ) ) {
 	/**
-	 * The Settings Helper class.
-	 *
-	 * @var Settings_Helper
+	 * Do the Settings.
 	 */
-	protected $settings_helper;
+	class Settings {
 
-	/**
-	 * The prefix for our settings keys.
-	 *
-	 * Gets set automatically from the Text Domain or can be set manually.
-	 * The prefix should not end with underscore `_`.
-	 *
-	 * @var string
-	 */
-	private $opts_prefix = '';
+		/**
+		 * The Settings Helper class.
+		 *
+		 * @var Settings_Helper
+		 */
+		protected $settings_helper;
 
-	/**
-	 * Settings constructor.
-	 */
-	public function __construct( $opts_prefix = 'tribe_ext' ) {
+		/**
+		 * The prefix for our settings keys.
+		 *
+		 * Gets set automatically from the Text Domain or can be set manually.
+		 * The prefix should not end with underscore `_`.
+		 *
+		 * @var string
+		 */
+		private $opts_prefix = '';
 
-		$this->settings_helper = new Settings_Helper();
+		/**
+		 * Settings constructor.
+		 */
+		public function __construct( $opts_prefix = 'tribe_ext' ) {
 
-		$this->set_options_prefix( $opts_prefix );
+			$this->settings_helper = new Settings_Helper();
 
-		// Add settings specific to OSM
-		add_action( 'admin_init', [ $this, 'add_settings' ] );
-	}
+			$this->set_options_prefix( $opts_prefix );
 
-	/**
-	 * Set the options prefix to be used for this extension's settings.
-	 *
-	 * Prefixes with `tribe_ext_` and ends with `_`.
-	 *
-	 * @param string $opts_prefix
-	 */
-	private function set_options_prefix( $opts_prefix = '' ) {
-		if ( empty( $opts_prefix ) ) {
-			$opts_prefix = str_replace( '-', '_', PLUGIN_TEXT_DOMAIN );
+			// Add settings specific to OSM
+			add_action( 'admin_init', [ $this, 'add_settings' ] );
 		}
-		$prefix = 'tribe_ext';
-		if ( 0 === strpos( $opts_prefix, $prefix ) ) {
-			$prefix = '';
+
+		/**
+		 * Set the options prefix to be used for this extension's settings.
+		 *
+		 * Prefixes with `tribe_ext_` and ends with `_`.
+		 *
+		 * @param string $opts_prefix
+		 */
+		private function set_options_prefix( $opts_prefix = '' ) {
+			if ( empty( $opts_prefix ) ) {
+				$opts_prefix = str_replace( '-', '_', PLUGIN_TEXT_DOMAIN );
+			}
+			$prefix = 'tribe_ext';
+			if ( 0 === strpos( $opts_prefix, $prefix ) ) {
+				$prefix = '';
+			}
+			$this->opts_prefix = $prefix . $opts_prefix . '_';
 		}
-		$this->opts_prefix = $prefix . $opts_prefix . '_';
-	}
-	/**
-	 * Given an option key, get this extension's option value.
-	 *
-	 * This automatically prepends this extension's option prefix so you can just do `$this->get_option( 'a_setting' )`.
-	 *
-	 * @see tribe_get_option()
-	 *
-	 * @param string $key
-	 *
-	 * @return mixed
-	 */
-	public function get_option( $key = '', $default = '' ) {
-		$key = $this->sanitize_option_key( $key );
-		return tribe_get_option( $key, $default );
-	}
-	/**
-	 * Get an option key after ensuring it is appropriately prefixed.
-	 *
-	 * @param string $key
-	 *
-	 * @return string
-	 */
-	private function sanitize_option_key( $key = '' ) {
-		$prefix = $this->get_options_prefix();
-		if ( 0 === strpos( $key, $prefix ) ) {
-			$prefix = '';
+
+		/**
+		 * Given an option key, get this extension's option value.
+		 *
+		 * This automatically prepends this extension's option prefix so you can just do `$this->get_option( 'a_setting' )`.
+		 *
+		 * @param string $key
+		 *
+		 * @return mixed
+		 * @see tribe_get_option()
+		 *
+		 */
+		public function get_option( $key = '', $default = '' ) {
+			$key = $this->sanitize_option_key( $key );
+
+			return tribe_get_option( $key, $default );
 		}
-		return $prefix . $key;
-	}
-	/**
-	 * Get this extension's options prefix.
-	 *
-	 * @return string
-	 */
-	public function get_options_prefix() {
-		return $this->opts_prefix;
-	}
-	/**
-	 * Given an option key, delete this extension's option value.
-	 *
-	 * This automatically prepends this extension's option prefix so you can just do `$this->delete_option( 'a_setting' )`.
-	 *
-	 * @param string $key
-	 *
-	 * @return mixed
-	 */
-	public function delete_option( $key = '' ) {
-		$key = $this->sanitize_option_key( $key );
-		$options = Tribe__Settings_Manager::get_options();
-		unset( $options[$key] );
-		return Tribe__Settings_Manager::set_options( $options );
-	}
 
-	/**
-	 * Adds the setting field to Events > Settings > General tab
-	 * The setting will appear above the "End of day cutoff" setting
-	 * (below the "Single event URL slug" setting)
-	 */
-	public function add_settings() {
-		$fields = [
-			$this->opts_prefix . 'custom_all_events_url' => [
-				'type'            => 'text',
-				'label'           => esc_html__( 'Custom "All Events" URL', PLUGIN_TEXT_DOMAIN ),
-				'tooltip'         => sprintf( esc_html__( 'Enter your custom URL, including "http://" or "https://", for example %s.', PLUGIN_TEXT_DOMAIN ), '<code>https://wpshindig.com/events/</code>' ),
-				'validation_type' => 'html',
-			]
-		];
+		/**
+		 * Get an option key after ensuring it is appropriately prefixed.
+		 *
+		 * @param string $key
+		 *
+		 * @return string
+		 */
+		private function sanitize_option_key( $key = '' ) {
+			$prefix = $this->get_options_prefix();
+			if ( 0 === strpos( $key, $prefix ) ) {
+				$prefix = '';
+			}
 
-		$this->settings_helper->add_fields(
-			$fields,
-			'general',
-			'multiDayCutoff',
-			true
-		);
-	}
+			return $prefix . $key;
+		}
 
-} // class
+		/**
+		 * Get this extension's options prefix.
+		 *
+		 * @return string
+		 */
+		public function get_options_prefix() {
+			return $this->opts_prefix;
+		}
+
+		/**
+		 * Given an option key, delete this extension's option value.
+		 *
+		 * This automatically prepends this extension's option prefix so you can just do `$this->delete_option( 'a_setting' )`.
+		 *
+		 * @param string $key
+		 *
+		 * @return mixed
+		 */
+		public function delete_option( $key = '' ) {
+			$key     = $this->sanitize_option_key( $key );
+			$options = Tribe__Settings_Manager::get_options();
+			unset( $options[ $key ] );
+
+			return Tribe__Settings_Manager::set_options( $options );
+		}
+
+		/**
+		 * Adds the setting field to Events > Settings > General tab
+		 * The setting will appear above the "End of day cutoff" setting
+		 * (below the "Single event URL slug" setting)
+		 */
+		public function add_settings() {
+			$fields = [
+				$this->opts_prefix . 'custom_all_events_url' => [
+					'type'            => 'text',
+					'label'           => esc_html__( 'Custom "All Events" URL', PLUGIN_TEXT_DOMAIN ),
+					'tooltip'         => sprintf( esc_html__( 'Enter your custom URL, including "http://" or "https://", for example %s.', PLUGIN_TEXT_DOMAIN ), '<code>https://wpshindig.com/events/</code>' ),
+					'validation_type' => 'html',
+				]
+			];
+
+			$this->settings_helper->add_fields( $fields, 'general', 'multiDayCutoff', true );
+		}
+
+	} // class
+}
